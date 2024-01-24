9c8e854d6d4df28d9a20fdead1ea54a7d2c8fb7b
DooxFonggg <phongcoi1235@gmail.com>
2024-01-24 18:17:56 +0700

    gpio_input

--- GPIO/input_ledc13/user/main.c
+++ GPIO/input_ledc13/user/main.c
@@ -19,39 +19,61 @@ void config_c13(void){
 	//reset CRH
 	GPIOC->CRH = 0x00000000;
 	//config mode 11 cnf 00
-	GPIOC->CRH |= 0x300000;
+	GPIOC->CRH |= 0x00300000;
 }
 void cofig_b9(void) {
     //reset CRH
 	GPIOB->CRH = 0x00000000;
 	//cofig chan
-	GPIOB->CRH |= 0x80;
+	GPIOB->CRH |= 0x00000080;
 	//bat outbut chan b90 de nhan tin hieu button
 	GPIOB->ODR |= (1<<9);
 }
 void gpio_setled(void) {
-	GPIOC->BSRR |=(1<<13); 
+	GPIOC->BSRR = (1<<13); 
 }
 void gpio_setlowlet(void){
-	GPIOC->BRR |=(1<<13);
+	GPIOC->BRR = (1<<13);
+}
+unsigned char gpio_getButton(void) {
+	if(GPIOB->IDR &(1<<9)) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
 
+unsigned char oldButton;
+unsigned char newButton;
+unsigned char ledStatus;
 
 int main() {
 	 rcc_config();
 	 config_c13();
 	 cofig_b9();
 	while(1) {
-	
-	/*if(GPIOC->IDR &(1<<9)) {
-		gpio_setled();
-	}
-	else {
-		gpio_setlowlet();
-	} */
-	gpio_setled();
+		/* newButton = gpio_getButton();
+		
+		if((newButton==0) && (oldButton==1)) {
+			ledStatus = !ledStatus;
+			if(ledStatus !=0) {
+				gpio_setled();
+			}
+			else{
+				gpio_setlowlet();
+			}
+		}
+		oldButton = newButton;
+		delay(200); */
+		
+		if(GPIOB->IDR &(1<<9)) gpio_setled();
+		else gpio_setlowlet();
+		delay(200);
+		
+	} 
+	/*gpio_setled();
 	delay(3000);
 	gpio_setlowlet();
-	delay(3000);
-}
+	delay(3000); */
 }
